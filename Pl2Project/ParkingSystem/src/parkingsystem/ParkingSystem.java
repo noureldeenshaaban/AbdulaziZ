5154f7ce77ca8c07bcd5a3203dd948c5b86ea32b
yousefmass <yousef.mas2005@gmail.com>
2025-12-16 15:40:05 +0200

    hi

---
+++ Pl2Project/ParkingSystem/src/parkingsystem/ParkingSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parkingsystem;
+
+/**
+ *
+ * @author youse
+ */
+public class ParkingSystem {
+
+  
+    
+}
